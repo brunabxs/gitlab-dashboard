541e35091b0e6086008ed9222afba0648619e7e4
brunabxs <brunabxsantos@gmail.com>
2017-06-30 09:30:14 -0300

    Reading data from GitLab using a private token

--- dashboard.js
+++ dashboard.js
@@ -1,3 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('dashboard').textContent = 'TESTE';
+$(document).ready(function () {
+  $.ajax({
+    url: 'https://gitlab.com/api/v4/projects?membership=true',
+    headers: {
+      'PRIVATE-TOKEN': '3oBioprn2GjJ52bdwBJ2'
+    },
+
+  })
+  .done(function(data, textStatus, jqXHR) {
+    _.each(data, function (project) {
+      $("#dashboard").append("<div class=\"project\">" + project.name + "</div>");
+    });
+    console.log(data);
+  })
+  .fail(function (jqXHR, textStatus, errorThrown) {
+    console.log(textStatus);
+    console.log(errorThrown);
+  })
 });

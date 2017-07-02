f784af007ea02660b32cbb3bfe63f07f2319d7fb
brunabxs <brunabxsantos@gmail.com>
2017-07-02 17:20:01 -0300

    Loading projects' merge requests information

--- app/models/gitlab-api.js
+++ app/models/gitlab-api.js
@@ -27,15 +27,50 @@ GitLabApi.prototype.getResource = function (resource) {
 GitLabApi.prototype.projects = function () {
     var self = this;
     return new Promise(function (resolve, reject) {
+        var projects;
         self.getResource('/projects?membership=true')
-            .then(function (projects) {
-                resolve(_.map(projects, function (project) {
+            .then(function (data) {
+                projects = _.map(data, function (project) {
                     return {
                         'id': project.id,
                         'name': project.name,
                         'url': project.web_url,
                     };
-                }));
+                });
+                return Promise.resolve(projects);
+            })
+            .then(function (data) {
+                var mergeRequestsPromises = [];
+                _.each(data, function (project) {
+                    mergeRequestsPromises.push(self.mergeRequests(project));
+                });
+                return Promise.all(mergeRequestsPromises);
+            })
+            .then(function (data) {
+                resolve(projects);
+            })
+            .catch(function (error) {
+                reject(error);
+            });
+    });
+};
+
+GitLabApi.prototype.mergeRequests = function (project) {
+    var self = this;
+    return new Promise(function (resolve, reject) {
+        self.getResource('/projects/' + project.id + '/merge_requests?state=opened')
+            .then(function (mergeRequests) {
+                project.mergeRequest = _.map(mergeRequests, function (mergeRequest) {
+                    return {
+                        'id': mergeRequest.id,
+                        'target_branch': mergeRequest.target_branch,
+                        'source_branch': mergeRequest.source_branch,
+                        'upvotes': mergeRequest.upvotes,
+                        'downvotes': mergeRequest.downvotes,
+                        'url': mergeRequest.web_url,
+                    };
+                });
+                resolve();
             })
             .catch(function (error) {
                 reject(error);

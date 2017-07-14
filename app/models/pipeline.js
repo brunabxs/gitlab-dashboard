0406471739595b59c1d55e6b3dd24e6c421f1a7c
brunabxs <brunabxsantos@gmail.com>
2017-07-14 02:13:09 -0300

    Changing the pipeline job selection method

--- app/models/pipeline.js
+++ app/models/pipeline.js
@@ -12,7 +12,7 @@ var Pipeline = function (id, scope, status, branch, commit, url) {
 Pipeline.get = function (gitlabApi, project) {
     var self = this;
     return new Promise(function (resolve, reject) {
-        gitlabApi.getResource('/projects/' + project.id + '/pipelines?sort=desc')
+        gitlabApi.getResource('/projects/' + project.id + '/pipelines?ref=master&sort=desc')
             .then(function (pipelines) {
                 if (pipelines.length > 0) {
                     return gitlabApi.getResource('/projects/' + project.id + '/pipelines/' + pipelines[0].id);
@@ -21,13 +21,17 @@ Pipeline.get = function (gitlabApi, project) {
             })
             .then(function (pipeline) {
                 if (pipeline) {
-                    return gitlabApi.getResource('/projects/' + project.id + '/pipelines/' + pipeline.id + '/jobs');
+                    return gitlabApi.getResource('/projects/' + project.id + '/pipelines/' + pipeline.id + '/jobs?scope[]=' + pipeline.status);
                 }
                 return Promise.resolve();
             })
-            .then(function (pipeline_jobs) {
-                if (pipeline_jobs) {
-                    var job = pipeline_jobs[0]; // TODO: pick a pipeline job
+            .then(function (jobs) {
+                if (jobs) {
+                    jobs = jobs.sort(function (jobLeft, jobRight) {
+                        return new Date(jobRight.started_at) - new Date(jobLeft.started_at);
+                    });
+
+                    var job = jobs[0];
                     var pipeline = job.pipeline;
                     project.pipeline(new Pipeline(pipeline.id, job.stage, pipeline.status, pipeline.ref, job.commit.short_id, ''));
                 }

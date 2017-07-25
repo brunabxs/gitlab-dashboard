5fbc4f041448eab7fabf9c8bf0769b48572d0625
brunabxs <brunabxsantos@gmail.com>
2017-07-25 19:37:05 -0300

    refactor: Use gulp task gulp-clean-css to minify CSS files

--- tasks/bundle.js
+++ tasks/bundle.js
@@ -1,3 +1,4 @@
+var cleanCSS = require('gulp-clean-css');
 var stream = require('event-stream');
 var gulp = require('gulp');
 var tasksConfig = require('../tasks.config.js');
@@ -7,7 +8,10 @@ var webpackConfig = require('../webpack.config.js');
 
 module.exports = function () {
     return stream.merge(
-        gulp.src(['app/**/icons/*.png', 'app/views/styles/*.css'])
+        gulp.src(['app/**/icons/*.png'])
+            .pipe(gulp.dest(tasksConfig.dist_dir)),
+        gulp.src('app/views/styles/*.css')
+            .pipe(cleanCSS({compatibility: 'ie8'}))
             .pipe(gulp.dest(tasksConfig.dist_dir)),
         gulp.src('')
             .pipe(webpack(webpackConfig))

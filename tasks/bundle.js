9a1068d229ddd0a27ca5aa1ade8576eaea84bb72
brunabxs <brunabxsantos@gmail.com>
2017-07-25 20:03:28 -0300

    refactor: Use SASS to create CSS files

--- tasks/bundle.js
+++ tasks/bundle.js
@@ -1,6 +1,6 @@
-var cleanCSS = require('gulp-clean-css');
-var stream = require('event-stream');
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var stream = require('event-stream');
 var tasksConfig = require('../tasks.config.js');
 var webpack = require('gulp-webpack');
 var webpackConfig = require('../webpack.config.js');
@@ -10,8 +10,8 @@ module.exports = function () {
     return stream.merge(
         gulp.src(['app/**/icons/*.png'])
             .pipe(gulp.dest(tasksConfig.dist_dir)),
-        gulp.src('app/views/styles/*.css')
-            .pipe(cleanCSS({compatibility: 'ie8'}))
+        gulp.src('app/views/styles/*.scss')
+            .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
             .pipe(gulp.dest(tasksConfig.dist_dir)),
         gulp.src('')
             .pipe(webpack(webpackConfig))

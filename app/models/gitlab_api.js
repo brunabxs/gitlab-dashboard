91c37a54c88a135eca16e5977c9523019a635af8
brunabxs <brunabxsantos@gmail.com>
2017-09-26 13:53:14 -0300

    feat: Load public, private and internal projects

--- app/models/gitlab_api.js
+++ app/models/gitlab_api.js
@@ -31,7 +31,28 @@ GitlabApi.prototype.getResource = function (resource) {
 
 GitlabApi.prototype.getProjects = function () {
     var self = this;
-    return self.getResource('/projects?membership=true');
+    var projects = [];
+
+    return new Promise(function (resolve, reject) {
+        self.getResource('/projects?visibility=public&membership=true')
+            .then(function (data) {
+                projects = projects.concat(data || []);
+                return self.getResource('/projects?visibility=private&membership=true');
+            })
+            .then(function (data) {
+                projects = projects.concat(data || []);
+                return self.getResource('/projects?visibility=internal&membership=true');
+            })
+            .then(function (data) {
+                projects = projects.concat(data || []);
+                resolve(projects);
+                return null;
+            })
+            .catch(function (error) {
+                reject(new Error(error));
+                return null;
+            });
+    });
 };
 
 GitlabApi.prototype.getRecentPipeline = function (projectId) {
@@ -39,7 +60,7 @@ GitlabApi.prototype.getRecentPipeline = function (projectId) {
     return new Promise(function (resolve, reject) {
         self.getResource('/projects/' + projectId + '/pipelines?ref=master&sort=desc')
             .then(function (pipelines) {
-                var pipeline = undefined;
+                var pipeline;
                 if (pipelines && pipelines.length > 0) {
                     return self.getResource('/projects/' + projectId + '/pipelines/' + pipelines[0].id);
                 }
@@ -61,7 +82,7 @@ GitlabApi.prototype.getRecentJobWithScope = function (projectId, pipelineId, job
     return new Promise(function (resolve, reject) {
         self.getResource('/projects/' + projectId + '/pipelines/' + pipelineId + '/jobs?scope[]=' + jobScope)
             .then(function (jobs) {
-                var job = undefined;
+                var job;
                 if (jobs && jobs.length > 0) {
                     jobs = jobs.sort(function (jobLeft, jobRight) {
                         return new Date(jobRight.started_at) - new Date(jobLeft.started_at);

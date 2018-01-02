599868bf8a0f36b8402f81ff02cabac7ee754089
brunabxs <brunabxsantos@gmail.com>
2018-01-02 13:02:00 -0200

    feat: Remove unnecessary vcs and project sections in dashboard

--- app/controllers/version_control_systems.js
+++ app/controllers/version_control_systems.js
@@ -1,7 +1,26 @@
+var _ = require('underscore');
+
 var Log = require('../models/log.js');
 
 module.exports = function (versionControlSystemsService, $scope, $interval) {
     $scope.vcss = versionControlSystemsService.vcss;
+
+    $scope.visibleBranches = function () {
+        var branches = [];
+        _.each($scope.vcss, function (vcs) {
+            _.each(vcs.projects.listAndUpdate(), function (project) {
+                if (project.visible) {
+                    _.each(project.branches.listAndUpdate(), function (branch) {
+                        if (branch.visible) {
+                            branch.project = project;
+                            branches.push(branch);
+                        }
+                    });
+                }
+            });
+        });
+        return branches;
+    };
 
     versionControlSystemsService.load();
 
